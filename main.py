f83e56614be50294ee66452139087bf3e9e33f0b
kevinbuckley <kbuckley17@gmail.com>
2023-09-08 21:39:49 -0400

    PoC loading a game file and generating images

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ from imagegenerator import ImageGenerator
 from dotenv import load_dotenv
 import asyncio
 
+import json
+
 
 async def main():
+    game_file = get_game_file()
+    await generate_images(game_file)
+
+
+def get_game_file():
+    with open('default_game_file.json', 'r') as f:
+        data = json.load(f)
+        return data
+
+
+async def generate_images(game_file):
     gen = ImageGenerator()
-    animals = ['praying mantis', 'lynx', 'hippo',
-               'giraffe', 'panda', 'baby goat', 'sloth']
 
     tasks = []
-    for animal in animals:
-        p1 = f"Magestic and fantasy looking {animal}.  To be used in a card game following the style of Hearthstone."
-        tasks.append(gen.get_image(p1, "animals", animal))
+    for card in game_file['deck']:
+        name = card['name']
+        p1 = f"Magestic and fantasy looking {name}.  To be used in a card game following the style of Hearthstone."
+        tasks.append(gen.get_image(p1, "game", name))
 
     results = await asyncio.gather(*tasks)
+
 
 if __name__ == "__main__":
     load_dotenv()
