1c50788d91898c663eabb037aaa657181d5ad928
kevinbuckley <kbuckley17@gmail.com>
2023-11-22 20:43:10 -0500

    path

--- web/src/scenes/MainScene.ts
+++ web/src/scenes/MainScene.ts
@@ -12,6 +12,7 @@ import {
 
 import { 
     cardWidth,
+    getBaseURL
 } from '../utils/DeckManagement';
 import { Boss } from '../mechanics/Boss';
 
@@ -41,17 +42,17 @@ export default class MainScene extends Phaser.Scene {
     this.state = new GameState(this.handleCardAttack, this.handleCardAttacked, this.handleCardPlayed);
       
     let request = new XMLHttpRequest();
-    request.open('GET', 'assets/game_file.json', false);  // `false` makes the request synchronous
+    request.open('GET', `${getBaseURL()}assets/game_file.json`, false);  // `false` makes the request synchronous
     request.send(null);
 
     if (request.status === 200) {
       const iCards = JSON.parse(request.responseText);
       const cards = iCards.filter((c:ICard) => !c.isBoss).map((c: ICard) => new Card(c.name, c.attack, c.health));
       const boss = iCards.filter((c:ICard) => c.isBoss).map((c: ICard) => new Boss(c.name, c.attack, c.health )).pop();
-      this.load.image(boss.name, `assets/${boss.name}.png`);
+      this.load.image(boss.name, `${getBaseURL()}assets/${boss.name}.png`);
 
       for (const card of cards) {
-        this.load.image(card.name, `assets/${card.name}.png`);
+        this.load.image(card.name, `${getBaseURL()}assets/${card.name}.png`);
       }
       this.state.create(cards, boss);
     }
--- web/src/utils/DeckManagement.ts
+++ web/src/utils/DeckManagement.ts
@@ -1,2 +1,11 @@
 export const cardWidth: number = 97;
 export const cardHeight: number = 130;
+
+
+export function getBaseURL(): string {
+    // Check if the URL contains the '/boss-battles/' subdirectory
+    if (window.location.pathname.includes('/boss-battles/')) {
+        return '/boss-battles/';
+    }
+    return ''; // Return empty string for local development
+}
