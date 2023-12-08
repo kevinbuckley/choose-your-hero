392a14b9d87f833dfd02f360adf3523b4491f07a
kevinbuckley <kbuckley17@gmail.com>
2023-12-08 18:26:08 -0500

    cleanup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,18 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      // Setup any necessary aliases
       '@': resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: '../docs/' // Set your desired output directory
+    outDir: '../docs/' 
   }
-  // You can include additional Vite specific configurations here
 });
